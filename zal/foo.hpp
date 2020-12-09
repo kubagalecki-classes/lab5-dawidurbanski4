ddb69aa1c31596c9c383b145f6a5a644015de98c
dawidurbanski4 <urbdadylbod@gmail.com>
2020-12-09 08:07:33 +0000

    zal

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "Human.hpp"
+#include<iostream>
 
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    for (Human & i : people) {
+        i.birthday();
+    }
+    std::vector<char> mons;
+    for (auto it = people.rbegin(); it != people.rend(); it++) {
+        if(it->isMonster())
+            mons.push_back('n');
+        else
+            mons.push_back('y');
+    }
+
+    return mons;
 }
